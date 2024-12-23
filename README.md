bc2cf2da0b8203ba2e774f6ebe7c849cd3f467c3
Ryan Peach <rgpeach10@gmail.com>
2024-12-23 13:38:13 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MdLLM
+
+This is a tool to take logseq style markdown (every item is a bulleted list, `[[wikilinks]]` exist which point to a `pages` directory, etc), and use graphrag to enable searching via an LLM for contextual question answering on your private notes.
---
+++ mdllm/__main__.py
@@ -0,0 +1,12 @@
+import typer
+from typer_config import use_toml_config
+
+app = typer.Typer()
+
+@app.command()
+@use_toml_config()  # MUST BE AFTER @app.command()
+def main(): 
+    raise NotImplementedError("This function is not implemented yet.")
+
+if __name__ == "__main__":
+    app()
