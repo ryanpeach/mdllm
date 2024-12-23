30de840364ec1e8cf824ab82da484dfc6c3928d3
Ryan Peach <rgpeach10@gmail.com>
2024-12-23 13:42:55 -0500

    First commit

--- mdllm/__main__.py
+++ mdllm/__main__.py
@@ -3,10 +3,12 @@ from typer_config import use_toml_config
 
 app = typer.Typer()
 
+
 @app.command()
 @use_toml_config()  # MUST BE AFTER @app.command()
-def main(): 
+def main():
     raise NotImplementedError("This function is not implemented yet.")
+
 
 if __name__ == "__main__":
     app()
--- mdllm/markdown.py
+++ mdllm/markdown.py
@@ -2,40 +2,45 @@ import markdown
 from markdown.extensions.wikilinks import WikiLinkExtension
 from markdown.inlinepatterns import InlineProcessor
 import xml.etree.ElementTree as etree
-from markdown.extensions.wikilinks import WikiLinkExtension
 
-HASH_WIKILINK_RE = r'(\s|^)\#\[\[([^\]]+)\]\]'
+HASH_WIKILINK_RE = r"(\s|^)\#\[\[([^\]]+)\]\]"
+
 
 class HashWikiLinkInlineProcessor(InlineProcessor):
     def handleMatch(self, m, data):
-        el = etree.Element('a')
+        el = etree.Element("a")
         tag = m.group(2).strip()
-        el.set('href', f'/tags/{tag}/')
-        el.set('class', 'tag')
-        el.text = f'#[[{tag}]]'
+        el.set("href", f"/tags/{tag}/")
+        el.set("class", "tag")
+        el.text = f"#[[{tag}]]"
         return el, m.start(0), m.end(0)
+
 
 class HashWikiLinkExtension(markdown.extensions.Extension):
     def extendMarkdown(self, md):
         hash_wikilink_pattern = HashWikiLinkInlineProcessor(HASH_WIKILINK_RE, md)
-        md.inlinePatterns.register(hash_wikilink_pattern, 'hash_wikilink', 175)
+        md.inlinePatterns.register(hash_wikilink_pattern, "hash_wikilink", 175)
 
-TAG_RE = r'(\s|^)#(\w+)\b'
+
+TAG_RE = r"(\s|^)#(\w+)\b"
+
 
 class TagInlineProcessor(InlineProcessor):
     def handleMatch(self, m, data):
-        el = etree.Element('a')
+        el = etree.Element("a")
         tag = m.group(2)
-        el.set('href', f'/tags/{tag}/')
-        el.set('class', 'tag')
-        el.text = f'#{tag}'
+        el.set("href", f"/tags/{tag}/")
+        el.set("class", "tag")
+        el.text = f"#{tag}"
         return el, m.start(0), m.end(0)
+
 
 class TagExtension(markdown.extensions.Extension):
     def extendMarkdown(self, md):
         tag_pattern = TagInlineProcessor(TAG_RE, md)
-        md.inlinePatterns.register(tag_pattern, 'tag', 175)
+        md.inlinePatterns.register(tag_pattern, "tag", 175)
 
 
-MD = markdown.Markdown(extensions=[WikiLinkExtension(), TagExtension(), HashWikiLinkExtension()])
-
+MD = markdown.Markdown(
+    extensions=[WikiLinkExtension(), TagExtension(), HashWikiLinkExtension()]
+)
